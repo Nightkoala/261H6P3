e64f00957f7a19a174c278f4820da4ca48244f12
Derek Brown <djb3718@rit.edu>
2014-11-20 21:13:12 -0500

    Added repo to eclipse and created java file

---
+++ CSCI261_hw6_p3/src/Robots.java
@@ -0,0 +1,23 @@
+/**
+ * Robots.java
+ * 
+ * @author	Derek Brown
+ *
+ * Purpose:	Given a grid with two robots and two objective spaces, determine the
+ * 			shortest number of moves to get a robot to each objective space.
+ */
+
+import java.util.Scanner;
+
+public class Robots {
+
+	// Hidden Data Members
+	
+	// Constructor
+	
+	// Methods
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end Robots class
